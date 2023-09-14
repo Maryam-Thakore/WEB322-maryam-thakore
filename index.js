aaaab6f45a5399b355a8496f31bd4bb0e51cba81
Benignant1 <maryammohamedt20@gmail.com>
2023-09-14 16:27:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello, world!");
+const EventEmitter = require('events');
+const trafficLight = new EventEmitter();
+
+const colors = ["Red", "Yellow", "Green"];
+let currentIndex = 0;
+
+function changeLight() {
+  const currentColor = colors[currentIndex];
+  trafficLight.emit('colorChange', currentColor);
+  currentIndex = (currentIndex + 1) % colors.length;
+}
+
+function startTrafficLight() {
+  setInterval(changeLight, 5000); // Red for 5 seconds
+  setTimeout(() => {
+    changeLight();
+  }, 2000); // Yellow for 2 seconds
+}
+
+startTrafficLight();
+
+trafficLight.on('colorChange', (currentColor) => {
+  console.log('The light just changed to', currentColor);
+});
